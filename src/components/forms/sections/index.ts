e7e24d9403d26a40af76c5dd073888cf1db5a8b6
Neveh Allon <34620850+nevehallon@users.noreply.github.com>
2021-06-29 02:50:19 +0300

    refactor and deploy

--- src/components/forms/sections/index.ts
+++
@@ -1,5 +0,0 @@
-import Section from "./Section";
-import Section2 from "./Section2";
-import SectionWrapper from "./SectionWrapper";
-
-export { Section, Section2, SectionWrapper };
--- src/interfaces/genericObjectProps.ts
+++
@@ -1,3 +0,0 @@
-export interface GOP {
-  [x: string]: any;
-}
